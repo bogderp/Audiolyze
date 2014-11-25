60b9e9b0dc532cb464f2a8bc02678fff6abaa870
bogdanpozderca <bogdanpozderca@gmail.com>
2014-11-25 01:01:50 -0500

    Gets the user's last played song
    Gets the user's last played song from the graphdata table.

---
+++ lastSong.php
@@ -0,0 +1,27 @@
+<?php
+	session_start();
+	$userID = $_SESSION['tableKey'];
+    $conn = new mysqli("localhost", "root", "21ooamlftw", "spotigraph");
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    } 
+
+
+    $result = $conn->query("SELECT publishTime, songName, artistName, 
+    	albumURL FROM " . $userID . "_musicdata ORDER BY publishTime DESC");
+
+    $lastArray = array();
+    if (mysqli_num_rows($result) > 0) {
+        // output data of each row
+            $row = mysqli_fetch_assoc($result);
+            array_push($lastArray, $row["albumURL"]);
+            $publishTime = substr($row["publishTime"], 0 ,22) . 
+            	':' . substr($row["publishTime"], 22);
+            array_push($lastArray, $publishTime);
+            array_push($lastArray, $row["songName"]);
+            array_push($lastArray, $row["artistName"]);
+            echo json_encode($lastArray);
+    } 
+    $conn->close();
+?>
