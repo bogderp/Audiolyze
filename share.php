ae8eec075d49c199fff9b005fcf63cb72b1c5489
bogdanpozderca <bogdanpozderca@gmail.com>
2014-12-25 02:21:04 -0500

    Added a Sharing Ability
    Added the Ability to share statistics with Facebook

---
+++ share.php
@@ -0,0 +1,157 @@
+<?php
+    session_start();
+    $conn = new mysqli("localhost", "publicuser", "srQ-kdq-5Jt-Mwp", "audiolyze");
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    } 
+
+    $userID = base_convert($_GET['i'], 18, 13);
+
+    $_SESSION['tableKey'] = $userID;
+
+    $userResult = $conn->query("SELECT name FROM userData WHERE userID='$userID'");
+    $name = mysqli_fetch_assoc($userResult)['name'];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Bogdan Pozderca">
+    <meta property="og:title" content='Audiolyzed Graph and Statistics' />
+    <meta property="og:image" content="http://www.audiolyze.com/imgFiles/metaImage.png">
+    <meta property="og:description" content="Check out my analyzed music history! You can even analyze your own for free!" />
+    <meta property="og:url" content="<?php echo 'http://www.audiolyze.com/share.php?i=' . $_GET['i']; ?>"/>
+    <link rel="shortcut icon" href="imgFiles/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="imgFiles/favicon.ico" type="image/x-icon">
+
+
+	<?php echo "<title>" . $name . "'s Audiolyze Graph" . "</title>"; ?>
+
+    <!-- Bootstrap core CSS -->
+    <link href="webFiles/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="webFiles/css/share.css">
+    <link href="webFiles/css/nv.d3.css" rel="stylesheet" type="text/css">
+    <link href="webFiles/css/graph.css" rel="stylesheet" type='text/css'>
+  </head>
+
+  <body>    
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.html">Audiolyze</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="index.html">Home</a></li>
+            <!--<li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>-->
+            <li><a href="FAQ.html">FAQ</a></li>
+            <li><a href="#requests" data-toggle="modal" data-target="#requests">Submit A Request</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+
+    <main class="bs-docs-masthead" id="content" role="main">
+         <div class="row">
+            <div class="col-lg-4 col-md-2 col-sm-1">
+            </div>       
+            <div id="welcome "class="col-lg-4 col-md-8 col-sm-10 col-xs-12">
+                <h1>Welcome to Audiolyze</h1>
+                <?php echo "<p class=\"lead\">" . $name . " decided to share their Audiolyzed statistics and graph with you! Enjoy!</p>";
+                      echo "<p class=\"lead2\">" . $name . " decided to share their Audiolyzed statistics and graph with you! To view the graph please visit this site on a computer. Enjoy!</p>"; 
+                ?>
+            </div>
+            <div class="col-lg-4 col-md-2 col-sm-1">
+            </div>
+        </div>  
+
+	    <div class='chart full' id='thegraph'>Top Artists<svg></svg></div>    
+	    
+	     <div class="bodyContainer">
+	        <div id="lastsong" style="display:none"></div>
+	        
+	        <div id="playstat" style="padding-top: 20px; display:none"></div>
+	    </div><!-- /.bodyContainer -->    
+        
+        <div class="fb-like-box" 
+            data-href="https://www.facebook.com/Audiolyze" 
+            data-width="100" data-colorscheme="dark" 
+            data-show-faces="true" data-header="false" 
+            data-stream="false" data-show-border="false"
+            style="display:none">
+        </div>      
+    </main>
+
+
+    <div class="modal fade" id="requests" tabindex="-1" role="dialog" aria-labelledby="requestLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+                <h4 class="modal-title" id="requestLabel">Request Form</h4>
+            </div>
+            <div class="modal-body">
+                <form class="form-horizontal" id="requestForm">
+                    <fieldset>
+
+                        <!-- name input-->
+                        <div class="form-group">
+                          <label class="col-md-2 control-label" for="submitName">Name</label>  
+                          <div class="col-md-10">
+                          <input id="submitName" name="submitName" type="text" placeholder="John Doe" class="form-control input-md">
+                            
+                          </div>
+                        </div>
+
+                        <!-- email input-->
+                        <div class="form-group">
+                          <label class="col-md-2 control-label" for="submitEMail">E-mail</label>  
+                          <div class="col-md-10">
+                          <input id="submitEMail" name="submitEMail" type="text" placeholder="jdoe@domain.com" class="form-control input-md">
+                            
+                          </div>
+                        </div>
+
+                        <!-- Textarea -->
+                        <div class="form-group">
+                          <label class="col-md-2 control-label" for="submitRequest">Request</label>
+                          <div class="col-md-10">                     
+                            <textarea class="form-control" id="submitRequest" name="submitRequest" 
+                                style="height:250px" placeholder="Maximum of 250 Words"></textarea>
+                          </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                            <button type="submit" class="btn btn-primary" id="send" name="send">Submit Request</button>
+                        </div>
+
+                    </fieldset>
+                </form>
+            </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="webFiles/dist/js/bootstrap.min.js"></script>
+    <script src="webFiles/js/d3.v3.js"></script>
+    <script src="webFiles/js/nv.d3.js"></script>
+    <script src="webFiles/js/shareFunctions.js" type="text/javascript"></script>
+  </body>
+</html>
+
---
+++ webFiles/js/shareFunctions.js
@@ -0,0 +1,142 @@
+(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=171546376229575&version=v2.0";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+
+
+function dynamicHeight() {
+    var height = $(window).height();
+    height = parseInt(height) + 'px';
+    $("main").css('height',height);
+}
+
+var colors = d3.scale.category20();
+var keyColor = function(d, i) {return colors(d.key)};
+
+function defaultChartConfig(container, data, useGuideline) {
+  if (useGuideline === undefined) useGuideline = true;
+  nv.addGraph(function() {
+    var chart;
+    chart = nv.models.stackedAreaChart()
+                  .useInteractiveGuideline(false)
+                  .x(function(d) { return d[0] })
+                  .y(function(d) { return d[1] })
+                  .color(keyColor);
+
+    chart.xAxis
+        .tickFormat(function(d) { return d3.time.format('%x')(new Date(d)) });
+
+    chart.yAxis
+        .tickFormat(d3.format(',.2f'));
+
+    d3.select('#' + container + ' svg')
+          .datum(data)
+        .transition().duration(500).call(chart);
+
+    nv.utils.windowResize(chart.update);
+
+    return chart;
+  });
+}
+
+function toD3Format() {
+    $.ajax({
+        url: "webFiles/php/toD3Format.php",
+        dataType: 'json',
+        success: function(data) {
+            var d3formatted = [ ];
+            for(i=0; i<data.length; i++){
+                obj = {
+                    "key": data[i][0],
+                    "values": [],
+                };
+                for(m=1; m<data[i].length; m++){
+                    obj.values.push(data[i][m]);
+                };
+                d3formatted.push(obj);
+            };
+            console.log(d3formatted);
+            defaultChartConfig("thegraph", d3formatted); 
+        }
+    });    
+};
+
+function topArtists() {
+    $.ajax({
+        url: "webFiles/php/topArtists.php",
+        dataType: 'json',
+        success: function(data) {
+            toD3Format();
+            otherArtists = data[data.length-4][1];
+            totalPlays = data[data.length-3];
+            newestDate = new Date(data[data.length-2][0]);
+            oldestDate = new Date(data[data.length-2][1]);
+            avgTimePlays = data[data.length-1];
+            document.getElementById('playstat').innerHTML = 
+                ("Top Five Artists' Number of Plays " +  
+                "from " + oldestDate.toDateString() + " to " + newestDate.toDateString() + "<br>" +
+                "1. " + data[0][0] + " was played " + data[0][1] + " times! " + 
+                    "That\'s " + (data[0][1]/totalPlays*100).toFixed(2) + "% of Plays <br>" + 
+                "2. " + data[1][0] + " was played " + data[1][1] + " times! " + 
+                    "That\'s " + (data[1][1]/totalPlays*100).toFixed(2) + "% of Plays <br>" + 
+                "3. " + data[2][0] + " was played " + data[2][1] + " times! " + 
+                    "That\'s " + (data[2][1]/totalPlays*100).toFixed(2) + "% of Plays <br>" + 
+                "4. " + data[3][0] + " was played " + data[3][1] + " times! " + 
+                    "That\'s " + (data[3][1]/totalPlays*100).toFixed(2) + "% of Plays <br>" + 
+                "5. " + data[4][0] + " was played " + data[4][1] + " times! " +
+                    "That\'s " + (data[4][1]/totalPlays*100).toFixed(2) + "% of Plays <br></br>" +
+                "On average, in the past " + totalPlays + " listens they listened to a song every " +
+                (avgTimePlays/60).toFixed(2) + " minutes!");
+            $("#thegraph").css('opacity',1);
+            $('#lastsong').fadeIn('slow', function () {
+                $('#playstat').fadeIn('slow');
+            });
+            $('.fb-like-box').fadeIn('slow');
+        }
+    }); 
+}
+ 
+function lastSong() {
+    $.ajax({
+            url: "webFiles/php/lastSong.php",
+            success: function(data) {
+                var lastSongArray = JSON.parse(data);
+                d = new Date(lastSongArray[1]);
+                document.getElementById('lastsong').innerHTML = 
+                    ("They last listened to music on " + 
+                    d.toDateString() + " at " + d.toLocaleTimeString() + 
+                    "<br>" +" You listened to : " + "<b>" + lastSongArray[2] + "</b>"
+                    + " by <b>" + lastSongArray[3] + "</b> <br>" +
+                    '<img src="' + lastSongArray[0] + '" height="100" width="100">');
+                    topArtists();
+            }
+        });
+}
+         
+$(document).ready(function(){
+    $(window).on('beforeunload', function() {
+        $(window).scrollTop(0);
+    });
+    dynamicHeight();
+    $(window).bind('resize', dynamicHeight);
+
+    $('#requestForm').on('submit',function(e) {
+        e.preventDefault();
+        requestData = $(this).serialize();
+        console.log(requestData);
+        $.ajax({
+                url: "webFiles/php/request.php",
+                type: "POST",
+                data: requestData,
+                success: function(data) {
+                    console.log(data);
+                    $("#requests").modal('hide');
+                }
+        });  
+    });
+    lastSong();
+}); 
